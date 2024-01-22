9a58b53dd43ed0749221a690fddb1c5c48ba1bc6
[Sumit] <[sumitadhikari166@gmail.com]>
2024-01-22 21:27:59 +0530

    initailization

---
+++ src/app/components/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Hero = () => {
+    return (
+        <div className="grid grid-cols-1 lg:grid-cols-12">
+            <section>
+                <h1 className='text-white mb-4 text-4xl lg:text-6xl font-extrabold'>
+                    Hello,I'm Sumit</h1>
+                <p className='text-[#ADB7BE] text-lg lg:text-xl'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet ad dolores dolor assumenda accusamus minus! Saepe eligendi odio, quasi,
+                    inventore suscipit distinctio ipsa dignissimos facilis possimus atque in, sit eum.
+                </p>
+            </section>
+        </div>
+    )
+}
+
+export default Hero
---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import '../app/components/Hero'
+import Hero from '../app/components/Hero'
+export default function Home() {
+  return (
+    <main className='flex min-h-screen flex-col bg-[#121212] px-12 py-4'>
+      <Hero />
+    </main>
+  )
+}
